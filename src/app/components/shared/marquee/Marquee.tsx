510bafafc552a160ac9ce620fa3dc6cee763f5be
dxdzdydxdzdy <dxdzdydxdzdy@gmail.com>
2025-04-09 14:15:10 +0300

    Первый блок + бегущая строка

---
+++ src/app/components/shared/marquee/Marquee.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import styles from './Marquee.module.scss'
+
+const Marquee = () => {
+	const items = Array.from({ length: 6 })
+
+	return (
+		<div className={styles.marquee}>
+			<div className={styles.container}>
+				{items.map((_, index) => (
+					<div key={index} className={styles.element}>
+						<Image width={78} height={49} alt='10%' src='/ten.svg' />
+						<p>СКИДКА НА ПЕРВЫЙ ЗАКАЗ ИЗ КИТАЯ*</p>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default Marquee
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,93 @@
+import Image from 'next/image'
+import styles from './Home.module.scss'
+import Marquee from './components/shared/marquee/Marquee'
 
 export default function Home() {
-  return (
-    <div>Hello</div>
-  );
+	return (
+		<>
+			<section className={styles.delivery}>
+				<div className={styles.container}>
+					<div className={styles.relative}>
+						<h1>
+							<strong>ДОСТАВКА ГРУЗА</strong> ОТ 30 КГ ИЗ КИТАЯ В РОССИЮ
+						</h1>
+						<p className={styles.absolute}>БЕЗ ПОСРЕДНИКОВ</p>
+					</div>
+					<div className={styles.truck}>
+						<Image
+							src={'/truck.svg'}
+							width={467}
+							height={433}
+							alt='Доставка оптом'
+						/>
+					</div>
+					<div className={styles.info}>
+						<div className={styles.top}>
+							<p>Полный цикл для селлеров</p>
+							<div className={styles.consultation}>БЕСПЛАТНАЯ КОНСУЛЬТАЦИЯ</div>
+						</div>
+						<div className={styles.mid}>
+							<div className={styles.content}>
+								<Image
+									width={66}
+									height={66}
+									src={'/china-flag.svg'}
+									alt='Флаг Китая'
+								/>
+								<p>Китай</p>
+							</div>
+							<div className={styles.lineContainer}>
+								<div className={styles.line1}></div>
+							</div>
+							<div className={styles.content}>
+								<Image
+									width={66}
+									height={66}
+									src={'/sc-logo-home.svg'}
+									alt='Лого SellerChina'
+								/>
+								<p>Sellerchina</p>
+							</div>
+							<div className={styles.lineContainer}>
+								<div className={styles.line2}></div>
+							</div>
+							<div className={styles.content}>
+								<Image
+									width={106}
+									height={76}
+									src={'/markets.svg'}
+									alt='Маркетплейсы'
+								/>
+								<p>Маркетплейсы</p>
+							</div>
+						</div>
+						<div className={styles.bot}>
+							<p>Оставьте свой контакт для быстрого расчёта</p>
+							<form method='post'>
+								<div>
+									<input
+										type='tel'
+										name='phone'
+										placeholder='Номер телефона'
+										required
+									/>
+									<Image
+										width={28}
+										height={28}
+										src={'/phone.svg'}
+										alt='tel'
+										className={styles.phone}
+									/>
+								</div>
+								<button type='submit'>Отправить</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section>
+				<Marquee />
+			</section>
+		</>
+	)
 }
