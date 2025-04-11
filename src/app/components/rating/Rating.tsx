0a5d01ab378443df8ed4a4780a343a3de1625ce5
dxdzdydxdzdy <dxdzdydxdzdy@gmail.com>
2025-04-11 17:01:03 +0300

    Сделан блок рейтинга и отзывов

--- src/app/components/rating/Rating.tsx
+++ src/app/components/rating/Rating.tsx
@@ -1,9 +1,49 @@
+import Image from 'next/image'
+import EasyCarousel from '../ratingCarousel/Carousel'
 import styles from './Rating.module.scss'
 
 const Rating = () => {
 	return (
 		<div className={styles.container}>
 			<h2>РЕЙТИНГ И ОТЗЫВЫ</h2>
+			<div className={styles.rating}>
+				<div className={styles.ratingItem}>
+					<div className={styles.header}>
+						<Image width={38} height={49} alt='' src={'/ratingY.svg'} />
+						<p className={styles.label}>Яндекс Карты</p>
+					</div>
+					<div className={styles.content}>
+						<span>4,8</span>
+						<div className={styles.stars}>
+							<Image width={165} height={37} alt='' src={'/ratingStars.svg'} />
+							<p>более 30 довольных клиентов</p>
+						</div>
+					</div>
+				</div>
+				<div className={styles.ratingItem}>
+					<div className={styles.header}>
+						<Image width={43} height={43} alt='' src={'/rating2gis.svg'} />
+						<p className={styles.label}>2ГИС</p>
+					</div>
+					<div className={styles.content}>
+						<span>5,0</span>
+						<div className={styles.stars}>
+							<Image width={165} height={37} alt='' src={'/ratingStars.svg'} />
+							<p>более 30 довольных клиентов</p>
+						</div>
+					</div>
+				</div>
+				<div className={styles.ratingItemOtzovik}>
+					<div className={styles.headerOtzovik}>
+						<Image width={38} height={49} alt='' src={'/ratingOtzovik.svg'} />
+						<p className={styles.label}>Отзовик</p>
+					</div>
+					<div className={styles.content}>
+						<span>4,7</span>
+					</div>
+				</div>
+			</div>
+			<EasyCarousel />
 		</div>
 	)
 }
