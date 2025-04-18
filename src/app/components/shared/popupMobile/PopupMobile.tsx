c14c6b5ddd75572bf8044cd1241e71c84b3be58a
dxdzdydxdzdy <dxdzdydxdzdy@gmail.com>
2025-04-18 15:54:51 +0300

    Форматирование ввода номера

--- src/app/components/shared/popupMobile/PopupMobile.tsx
+++ src/app/components/shared/popupMobile/PopupMobile.tsx
@@ -4,7 +4,16 @@ import { useEffect, useRef, useState } from 'react'
 import styles from './PopupMobile.module.scss'
 
 const formatPhone = (value: string) => {
-	const digits = value.replace(/\D/g, '')
+	let digits = value.replace(/\D/g, '')
+
+	if (digits.startsWith('7') && digits.length >= 11) {
+		digits = digits.substring(1)
+	} else if (digits.startsWith('8') && digits.length >= 11) {
+		digits = digits.substring(1)
+	} else if (digits.startsWith('9') && digits.length > 10) {
+		digits = digits.substring(digits.length - 10)
+	}
+
 	let formatted = ''
 
 	if (digits.length > 0) {
