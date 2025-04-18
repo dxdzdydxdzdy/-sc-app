b0b779aeef1c9f256ec050f988c064303ff4fafe
dxdzdydxdzdy <dxdzdydxdzdy@gmail.com>
2025-04-18 10:03:00 +0300

    Тест иконки whatsApp

--- src/app/components/shared/Header.tsx
+++ src/app/components/shared/Header.tsx
@@ -61,12 +61,7 @@ const Header = () => {
 									<Image src='tg.svg' alt='tg' width={14} height={13} />
 								</Link>
 								<Link href='/'>
-									<Image
-										src='wu-min.svg'
-										alt='whatsapp'
-										width={17}
-										height={17}
-									/>
+									<Image src='wu.webp' alt='whatsapp' width={17} height={17} />
 								</Link>
 							</div>
 
