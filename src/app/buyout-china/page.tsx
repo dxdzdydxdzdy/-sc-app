872ff1f8260f89eaf381d3562193f189aab18603
dxdzdydxdzdy <dxdzdydxdzdy@gmail.com>
2025-05-07 17:53:36 +0300

    Создание и адаптив страницы BuyoutChinaHowWeWork

--- src/app/buyout-china/page.tsx
+++ src/app/buyout-china/page.tsx
@@ -1,3 +1,6 @@
+import AnyQuestion from '../components/shared/anyQuestion/AnyQuestion'
+import Socials from '../components/socials/Socials'
+import SpecialRating from '../special-line/components/special-rating/SpecialRating'
 import BuyoutChinaHero from './components/buyout-china-hero/BuyoutChinaHero'
 import BuyoutChinaHowWeWorks from './components/buyout-china-how-we-works/BuyoutChinaHowWeWorks'
 
@@ -9,6 +12,15 @@ const page = () => {
 			</section>
 			<section>
 				<BuyoutChinaHowWeWorks />
+			</section>
+			<section>
+				<SpecialRating />
+			</section>
+			<section>
+				<AnyQuestion />
+			</section>
+			<section>
+				<Socials />
 			</section>
 		</>
 	)
